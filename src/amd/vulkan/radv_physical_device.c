37aab592a8559c870f0d9c28dfa586d099b5e6c2
hmtheboy154 <buingoc67@gmail.com>
2024-06-07 18:14:05 -0400

    driconf: Add an option to override Vulkan's deviceName
    There is currently an Android game called Gakuen iDOLM@STER that
    check Vulkan's deviceName to see if the hardware is capable of
    running highest settings by using whitelist.
    
    Create "force_vk_devicename" driconf to spoof deviceName to deal
    with cases like this. I bring this option to anv, hasvk, radv, nvk,
    venus as these will be used by Android-x86 or BlissOS. Additionally
    I bring this to v3dv too as I have a Raspberry Pi 5
    
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- src/amd/vulkan/radv_physical_device.c
+++ src/amd/vulkan/radv_physical_device.c
@@ -2023,7 +2023,8 @@ radv_get_physical_device_properties(struct radv_physical_device *pdev)
 
    struct vk_properties *p = &pdev->vk.properties;
 
-   strcpy(p->deviceName, pdev->marketing_name);
+   strcpy(p->deviceName, (strlen(instance->drirc.force_vk_devicename) > 0) ?
+   instance->drirc.force_vk_devicename : pdev->marketing_name);
    memcpy(p->pipelineCacheUUID, pdev->cache_uuid, VK_UUID_SIZE);
 
    memcpy(p->deviceUUID, pdev->device_uuid, VK_UUID_SIZE);
